af9eae4a2ef5df570a5aac55135966aaa54608c6
Diego Silva <117883859+Arxxh@users.noreply.github.com>
2026-09-16 22:16:35 -0600

    Add HackerRank Easy - Java Datatypes

---
+++ HackerRank/Easy/Java Datatypes/solution.java
@@ -0,0 +1,43 @@
+// HackerRank Problem: Java Datatypes
+// Link: https://www.hackerrank.com/challenges/java-datatypes/problem
+// Difficulty: Easy
+// Language: java15
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        
+        for (int i = 0; i < t; i++){
+            
+            try {
+            long x = scanner.nextLong();
+            
+            System.out.println(x + " can be fitted in:");
+            
+            if (x >= -128 && x <= 127) {
+                System.out.println("* byte");
+                }
+            if (x >= -32768 && x <= 32767) {
+                System.out.println("* short");
+                }
+            if (x >= -2147483648 && x <= 2147483647){
+                System.out.println("* int");
+                }
+            System.out.println("* long");
+            
+            } catch (Exception e) {
+                String numero = scanner.next();
+                
+                 System.out.println(numero + " can't be fitted anywhere.");
+            }
+        }
+        
+        scanner.close();
+}      
+        
+    }
